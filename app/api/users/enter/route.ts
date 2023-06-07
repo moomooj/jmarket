38d9d64d937fdbe1fa6c6d195fac841bc99a89c1
moomooj <wndud36395@gmail.com>
2023-06-07 10:20:43 +0900

    token model 생성, token-user model 관계 생성, token connectOrCreate 추가

--- app/api/users/enter/route.ts
+++ app/api/users/enter/route.ts
@@ -3,54 +3,27 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function POST(req: Request) {
   const { phone, email } = await req.json();
-  const payload = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : { email };
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
 
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
 
-  /*
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("유저 찾았음!");
-    if (!user) {
-      console.log("유저 못찾아서 만들겠음!");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-  }
+  console.log(token);
 
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log("유저 찾았음!!");
-    if (!user) {
-      console.log("유저 못찾아서 만들겠음!!");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-  }*/
   return NextResponse.json({ ok: true });
 }
