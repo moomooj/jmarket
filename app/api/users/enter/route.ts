c56bfb6e4cf0edfe85816b2c917add0d86279e39
moomooj <wndud36395@gmail.com>
2023-06-07 11:20:06 +0900

    ResponseBody type 작성, status 추가

--- app/api/users/enter/route.ts
+++ app/api/users/enter/route.ts
@@ -1,11 +1,11 @@
-import { client } from "@/libs/server/client";
+import { ResponseBody, client } from "@/libs/server/client";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: Request): Promise<NextResponse<ResponseBody>> {
   const { phone, email } = await req.json();
-  const user = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return NextResponse.json({ ok: false }, { status: 400 });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
-
   const token = await client.token.create({
     data: {
       payload,
@@ -23,7 +23,5 @@ export async function POST(req: Request) {
     },
   });
 
-  console.log(token);
-
-  return NextResponse.json({ ok: true });
+  return NextResponse.json({ ok: true }, { status: 200 });
 }
--- libs/server/client.ts
+++ libs/server/client.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
+export type ResponseBody = { ok: boolean; [key: string]: any };
+
 export const client = new PrismaClient();
