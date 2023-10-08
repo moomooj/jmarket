ce4592696c5a855760d2dd6297cd94cada968e06
moomooj <wndud36395@gmail.com>
2023-10-08 15:18:49 +0900

    email test

--- libs/server/email.ts
+++ libs/server/email.ts
@@ -3,12 +3,14 @@ import nodemailer from "nodemailer";
 const smtpTransport = nodemailer.createTransport({
   service: "Naver",
   host: "smtp.naver.com",
-  port: 465,
+  port: 587,
   auth: {
     user: process.env.MAIL_ID,
     pass: process.env.MAIL_PASSWORD,
   },
-  secure: true,
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default smtpTransport;
